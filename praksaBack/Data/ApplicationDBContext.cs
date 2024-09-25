b5f1de007e181e677abe25248d85f0c8add73679
Marko Nisic <marko.nisic@page.ba>
2024-09-25 16:24:36 +0200

    init migration

---
+++ praksaBack/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using praksaBack.Models;
+
+namespace praksaBack.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
--- praksaBack/Models/Stock.cs
+++ praksaBack/Models/Stock.cs
@@ -4,19 +4,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace praksaBack.Models
 {
-    //[Route("api/[controller]")]
-    //[ApiController]
-    public class Stock : ControllerBase
+    public class Stock
     {
         public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
 
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Purchase { get; set; }
+        public double Purchase { get; set; }
 
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal LastDiv { get; set; }
+        public double LastDiv { get; set; }
 
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }

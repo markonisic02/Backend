76ee9236b05f53cf167d8485e706115adc9a186d
Marko Nisic <marko.nisic@page.ba>
2024-09-25 14:40:44 +0200

    Add project files.

---
+++ praksaBack/Models/Comment.cs
@@ -0,0 +1,8 @@
+﻿namespace praksaBack.Models
+{
+    public class Comment
+    {
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
---
+++ praksaBack/Models/Stock.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace praksaBack.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Stock : ControllerBase
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment> { };
+    }
+}
